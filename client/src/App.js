539095c64f7a3922f3901b65798ef4d084efe362
shahid1223 <ms413959@gmail.com>
2022-09-29 00:19:59 +0530

    task completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Signup from './pages/Signup';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 const App = () => {
+  const data = useSelector(state => state);
+  useEffect(() => {
+    if (data.tokenFromLogin !== null) {
+      console.log("Loged in")
+    }
+    if (data.tokenFromSiginUp !== null) {
+      console.log("Registered successfully")
+    }
+  }, [data])
   return (
     <Fragment>
-      <Signup />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>strokeLinecap
     </Fragment>
   );
 }
